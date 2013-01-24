53bad41252f76b77c39f75e9f346ee76f595739c
Carlos Rodriguez <carlos@s8f.org>
2013-01-24 14:53:42 -0800

    only deploy master

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,36 @@
 var config = require('../../config.js');
 
 exports.index = function(req, res){
-	res.json( { status: 'ok' });
+        res.json( { status: 'ok' });
 };
 
 exports.favicon = function(req, res){
-	res.writeHead(404);
-	res.end();
+        res.writeHead(404);
+        res.end();
 };
 
 exports.github = function(req, res){
-	authorizedIps = config.security.authorizedIps;
-	var payload = req.body.payload;
-	if (payload && authorizedIps.indexOf(req.ip) >= 0) {
-		console.log('It\'s payload time!');
-		myExec(config.action.exec);
-		res.writeHead(200);
-	} else {
-		res.writeHead(403);
-	}
-	res.end();
+        authorizedIps = config.security.authorizedIps;
+        var payload = req.body.payload;
+        if (payload && authorizedIps.indexOf(req.ip) >= 0) {
+                payload = JSON.parse(payload);
+                console.log('payload', payload);
+                if (payload.ref === 'refs/heads/master') {
+                        myExec(config.action.exec);
+                }
+                res.writeHead(200);
+        } else {
+                res.writeHead(403);
+        }
+        res.end();
 };
 
 var myExec = function(line) {
     var exec = require('child_process').exec;
-	var execCallback = function (error, stdout, stderr) {
-	    if (error !== null) {
-	      console.log('exec error: ' + error);
-	    }
-	}
-	var child = exec(line, execCallback);
+        var execCallback = function (error, stdout, stderr) {
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+        }
+        var child = exec(line, execCallback);
 }
