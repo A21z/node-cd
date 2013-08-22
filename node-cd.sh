277d4cd9ff18dfe15a6b753b3b672bff641a11e8
guanbo <guanbo2002@gmail.com>
2013-08-22 20:58:23 +0800

    Update: templete shell script

--- node-cd.sh
+++
@@ -1,3 +0,0 @@
-echo "It's payback time!"
-# Place your script here
-# ex: stop server, git pull, restart server
---
+++ node-cd.templete.sh
@@ -0,0 +1,20 @@
+echo "Stop server"
+echo "====================================="
+# forever stop server.js
+
+echo "Update Code"
+echo "====================================="
+# cd ~/expro-future
+# git pull
+
+echo "Install dependency"
+# cd passpro-crm/app
+# bower install
+
+# cd ../../passpro
+# npm install
+
+echo "Restart server"
+echo "====================================="
+# forever start server.js
+# cd
