150b06199a9e956467f6ae9c212e9f2854f283fe
Alfred Lausignorekai@gmail.com <signorekai@gmail.com>
2015-11-04 18:40:34 +0800

    express 4.0 updates

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ var fs = require('fs');
 var routes = require('./routes.js');
 var config = require('../config.js');
 var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-app.configure(function(){
   app.set('port', process.env.WWW_PORT || config.server.port);
+  app.use(morgan('combined'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.static(path.join(__dirname + 'public')));
-});
 
+/*
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -21,6 +25,7 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
+*/
 
 //app.get('/routes', routes.__allRoutes);
 app.get('/', routes.index.index);
